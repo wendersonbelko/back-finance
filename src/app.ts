54a85e06e1639d1fd7c8480ab406d0e5a5e63613
wenderson belko <wenderson.belko@ufly.com.br>
2025-05-02 21:12:33 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,18 @@ import { calendarRoutes } from './modules/calendar/calendar.routes'
 export const app = Fastify({ logger: true })
 
 app.register(cors, {
-  origin: 'http://5.161.229.88:4000/',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'Accept',
+    'Origin',
+    'User-Agent',
+  ],
+  
 })
 
 app.register(authRoutes, { prefix: '/auth' })
